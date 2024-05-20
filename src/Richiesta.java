00f8fe30b89aab698c1fb01f68e9c592f70a6513
Stefano Rubinetti <stefano.rubinetti@gmail.com>
2024-05-20 12:37:06 +0200

    v1

---
+++ src/Richiesta.java
@@ -0,0 +1,22 @@
+public class Richiesta 
+{
+    public static void main(String[] args) 
+    {
+        //Chiedere all'utente in INGLESE di inserire la lingua del programma
+        //ITA-una a vostra scelta
+        
+        //Leggere il file associato a quella lingua come ho fatto in Esempio
+        
+        //Chiedere all'utente di inserire una parola in inglese tra
+        //fork
+        //spoon
+        //cup
+        //door
+        //dog
+
+        //stampare la traduzione nella lingua scelta dall'utente di tale parola
+        //se inserisce una parola diversa da quelle supportate stampare "IMPOSSIBILE TRADURRE"
+        //nella lingua scelta dall'utente
+        //se nelle mappe si fa mappa.get("chiaveNonEsistente") lui restituisce null
+    }
+}
